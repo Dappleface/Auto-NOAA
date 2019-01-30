05d3ca2b32823cc6572845a1d5fbdcbc4cf2e986
Hamish <Dappleface@gmail.com>
2019-01-30 22:05:30 +1100

    yeah its getting there

--- setup.sh
+++ setup.sh
@@ -4,3 +4,8 @@ echo "this script requires root"
 echo "testing for sudo"
 timeout 2 sudo id && echo "Sudo Detected, Continuing" || echo "please try again with sudo"
 echo "Updating and Installing required packages"
+
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install pip python3 
+echo "done"
