1db7804350a3b395d73c1681b58a5731f4b51df7
Hamish <Dappleface@gmail.com>
2019-01-31 09:28:36 +1100

    more fixes

--- dynamic.py
+++ dynamic.py
@@ -1,7 +1,18 @@
-from skyfield.api import Topos
+from skyfield.api import *
 
 planets = load('de421.bsp')
 earth, mars = planets['earth'], planets['mars']
+
+ts = load.timescale()
+t = ts.now()
+position = earth.at(t).observe(mars)
+ra, dec, distance = position.radec()
+
+print(ra)
+print(dec)
+print(distance)
+
+print("Az +EL")
 
 boston = earth + Topos('42.3583 N', '71.0636 W')
 astrometric = boston.at(t).observe(mars)
