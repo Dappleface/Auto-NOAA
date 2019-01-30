eb7f62840d64260f6a72a5291b501b508bc94512
Hamish <Dappleface@gmail.com>
2019-01-31 09:27:12 +1100

    fixes

--- dynamic.py
+++ dynamic.py
@@ -1,5 +1,8 @@
 from skyfield.api import Topos
 
+planets = load('de421.bsp')
+earth, mars = planets['earth'], planets['mars']
+
 boston = earth + Topos('42.3583 N', '71.0636 W')
 astrometric = boston.at(t).observe(mars)
 alt, az, d = astrometric.apparent().altaz()
