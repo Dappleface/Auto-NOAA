7700faec328dd446989305af88b794665980c812
Hamish <Dappleface@gmail.com>
2019-02-24 11:14:51 +1100

    Update dynamic.py

--- dynamic.py
+++ dynamic.py
@@ -1,22 +1,76 @@
-from skyfield.api import load
-
-planets = load('de421.bsp')
-earth, mars = planets['earth'], planets['mars']
-
+from skyfield.api import Topos, load, now
 ts = load.timescale()
 t = ts.now()
-position = earth.at(t).observe(mars)
-ra, dec, distance = position.radec()
 
-print(ra)
-print(dec)
-print(distance)
+stations_url = 'http://celestrak.com/NORAD/elements/noaa.txt'
 
-print("Az +EL")
+def satload(satname):
+    satellites = load.tle(stations_url)
+    satellite = satellites[satname]
+    days = t - satellite.epoch
+    if abs(days) > 1:
+        satellites = load.tle(stations_url, reload=True)
+        satellite = satellites[satname]
+        print('{:.3f} days away from epoch'.format(days))
+    print(satellite)
 
-boston = earth + Topos('42.3583 N', '71.0636 W')
-astrometric = boston.at(t).observe(mars)
-alt, az, d = astrometric.apparent().altaz()
+def sattrack(satname):
+    satellites = load.tle(stations_url)
+    satellite = satellites[satname]
+    geocentric = satellite.at(t)
+    print(geocentric.position.km)
+    subpoint = geocentric.subpoint()
+    print('latitude: ', subpoint.latitude)
+    print('longitude: ', subpoint.longitude)
+    print('Elevation (m): ', int(subpoint.elevation.m))
 
-print(alt)
-print(az)
+
+
+
+
+def dms_to_dd(d, m, s):
+    dd = d + float(m)/60 + float(s)/3600
+    return dd
+print("noaa-15 current location from current position:")
+print("current direction W-E (latitude)")
+print(dms_to_dd(-17, 33, 08.2))
+print("current direction N-S (longiatude)")
+print(dms_to_dd(-49, 32, 38.6))
+#=======================================================================
+print("=====================================")
+print(r"""
+          ooo
+         / : \
+        / o0o \
+  _____"~~~~~~~"_____
+  \+###|U * * U|###+/
+   \...!(.>..<)!.../
+    ^^^^o|   |o^^^^
+#+=====}:^^^^^:{=====+#
+ .____  .|!!!|.  ____.
+ |#####:/" " "\:#####|
+ |#####=|NO-AA|=#####|
+ |#####>\_____/<#####|
+  ^^^^^   | |   ^^^^^
+          o o
+ """)
+
+print("=====================================")
+
+
+print("satload:   ")
+print()
+satload('NOAA 15 [B]')
+print()
+satload('NOAA 18 [B]')
+print()
+satload('NOAA 19 [+]')
+print()
+
+print("print sat trak:   ")
+print()
+sattrack('NOAA 15 [B]')
+print()
+sattrack('NOAA 18 [B]')
+print()
+sattrack('NOAA 19 [+]')
